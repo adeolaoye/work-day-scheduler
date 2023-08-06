3135a963f00fe17d8b38e895ba7137d970dc24e0
ADE OLAOYE <adeolaoye56@gmail.com>
2023-08-06 16:51:36 +0100

    edit ids in index and script files

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 $(document).ready(function () { // once page loads perform 
     //display current day & time.
-    $("#currentDay").text(dayjs().format("MMMM D YYYY, h:mm:ss A"));
+    var currentDay = $('#currentDay');
+    currentDay.text(dayjs().format("D MMMM YYYY, h:mm:ss A"));
 
     //assign saveBtn click listener for user input and time stamp??
-    $(".saveBtn").on("click", function () {
+    var saveBtn = $(".saveBtn")
+    saveBtn.on("click", function () {
       //get nearby values.
       var text = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
@@ -13,16 +15,16 @@ $(document).ready(function () { // once page loads perform
     });
 
     //load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
-    $("#hour8 .description").val(localStorage.getItem("hour8"));
-    $("#hour9 .description").val(localStorage.getItem("hour9"));
-    $("#hour10 .description").val(localStorage.getItem("hour10"));
-    $("#hour11 .description").val(localStorage.getItem("hour11"));
-    $("#hour12 .description").val(localStorage.getItem("hour12"));
-    $("#hour13 .description").val(localStorage.getItem("hour13"));
-    $("#hour14 .description").val(localStorage.getItem("hour14"));
-    $("#hour15 .description").val(localStorage.getItem("hour15"));
-    $("#hour16 .description").val(localStorage.getItem("hour16"));
-    $("#hour17 .description").val(localStorage.getItem("hour17"));
+    $("#timeBlockHour8 .description").val(localStorage.getItem("timeBlockHour8"));
+    $("#timeBlockHour9 .description").val(localStorage.getItem("timeBlockHour9"));
+    $("#timeBlockHour10 .description").val(localStorage.getItem("timeBlockHour10"));
+    $("#timeBlockHour11 .description").val(localStorage.getItem("timeBlockHour11"));
+    $("#timeBlockHour12 .description").val(localStorage.getItem("timeBlockHour12"));
+    $("#timeBlockHour13 .description").val(localStorage.getItem("timeBlockHour13"));
+    $("#timeBlockHour14 .description").val(localStorage.getItem("timeBlockHour14"));
+    $("#timeBlockHour15 .description").val(localStorage.getItem("timeBlockHour15"));
+    $("#timeBlockHour16 .description").val(localStorage.getItem("timeBlockHour16"));
+    $("#timeBlockHour17 .description").val(localStorage.getItem("timeBlockHour17"));
 
     function hourTracker() {
       //get current number of hours.
@@ -30,15 +32,15 @@ $(document).ready(function () { // once page loads perform
 
       // loop over time blocks
       $(".time-block").each(function () {
-        var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockHour, currentHour);
+        var timeBlockHour = parseInt($(this).attr("id").split("timeBlockHour")[1]);
+        console.log(timeBlockHour, currentHour);
 
         //check if we've moved past this time, click into css/html given classes of past, present, or future
-        if (blockHour < currentHour) {
+        if (timeBlockHour < currentHour) {
           $(this).addClass("past");
           $(this).removeClass("future");
           $(this).removeClass("present");
-        } else if (blockHour === currentHour) {
+        } else if (timeBlockHour === currentHour) {
           $(this).removeClass("past");
           $(this).addClass("present");
           $(this).removeClass("future");
